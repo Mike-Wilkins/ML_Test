b211106f0932f02668d94bb24325b343faa405f1
Mike <michael-wilkins@sky.com>
2020-04-11 14:40:21 +0100

    Add project files.

---
+++ ML_Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ML_TestML.Model;
+
+
+namespace ML_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Add input data
+            var input = new ModelInput();
+            input.Trip_distance = 100;
+            input.Trip_time_in_secs = 1000;
+            input.Passenger_count = 3;
+            input.Payment_type = "CSH";
+            input.Rate_code = 1;
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"Prediction: ${result.Score}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
